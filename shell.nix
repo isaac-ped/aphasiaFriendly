0eaf0542d3e292214e4e60f2e6005e7bce864b44
Isaac Pedisich <isaac.pedisich@gmail.com>
2023-09-30 17:31:32 -0700

    Basic nix setup

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.hello
+    pkgs.poetry
+    pkgs.python311
+
+    # keep this line if you use bash
+    pkgs.bashInteractive
+  ];
+}
