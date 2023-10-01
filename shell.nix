1ac8ae782dbc12bc05b3fac9682ee43aa2809f94
Deborah Levy <dlevy@Deborahs-MacBook-Pro.local>
2023-09-30 18:23:17 -0700

    Comment to shell.nix

--- shell.nix
+++ shell.nix
@@ -5,10 +5,15 @@ pkgs.mkShell {
     pkgs.hello
     pkgs.poetry
     pkgs.python311
+
+    # These are necessary to get pytorch and transformers to work properly
     pkgs.rustup
     pkgs.cargo
     pkgs.zstd
     pkgs.iconv
+    pkgs.darwin.apple_sdk.frameworks.Security
+    pkgs.pkg-config
+    pkgs.openssl
 
     # keep this line if you use bash
     pkgs.bashInteractive
