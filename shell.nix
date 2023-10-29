d05c2853159847c492c4cf424a9ba23acd95627b
Isaac Pedisich <isaac@pipe.com>
2023-10-29 17:51:45 +0100

    Couple more cleanups

--- shell.nix
+++ shell.nix
@@ -2,19 +2,11 @@
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.hello
     pkgs.poetry
     pkgs.python311
+    # A command runner that I like
+    # https://github.com/casey/just
     pkgs.just
-
-    # These are necessary to get pytorch and transformers to work properly
-    pkgs.rustup
-    pkgs.cargo
-    pkgs.zstd
-    pkgs.iconv
-    pkgs.darwin.apple_sdk.frameworks.Security
-    pkgs.pkg-config
-    pkgs.openssl
 
     # keep this line if you use bash
     pkgs.bashInteractive
