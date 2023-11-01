c0f7eb1cb0ce03f31172d07cae4d9f7221f969d7
Isaac Pedisich <isaac@pipe.com>
2023-11-01 14:54:34 +0100

    Add imgcat to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+
+  imgcat=pkgs.writeShellScriptBin "imgcat" (builtins.readFile ./bin/imgcat);
+in
 
 pkgs.mkShell {
   buildInputs = [
+    imgcat
     pkgs.poetry
     pkgs.python311
     # A command runner that I like
     # https://github.com/casey/just
     pkgs.just
+
+    # For svg -> png conversion
+    pkgs.imagemagick
 
     # keep this line if you use bash
     pkgs.bashInteractive
