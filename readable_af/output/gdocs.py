9e3543776950ec30e8d4cacbc62bb2db363b3977
deb-levy <deborah.levy@princeton.edu>
2025-04-04 15:53:23 -0400

    Update gdocs.py
    Attempt to add watermark with link

--- readable_af/output/gdocs.py
+++ readable_af/output/gdocs.py
@@ -96,7 +96,15 @@ class GoogleDocGenerator:
                     "name": f"TESTING: {summary.metadata.simplified_title}",
                     "parents": [folder],
                     "mimeType": "application/vnd.google-apps.document",
-                },
+                    'createFooter': {'type': 'DEFAULT'},
+                    'insertText': {'location': {'segmentId': 'footer','index': 0},
+                                   'text': 'Summary created with Article Friend'},
+                    'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
+                                        'textStyle': {'italic': True},'fields': 'italic'},
+                    'updateParagraphStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
+                                             'paragraphStyle': {'alignment': 'END'},'fields': 'alignment'}},
+                    'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 21,'endIndex': 35},
+                                        'textStyle': {'link': {'url': 'https://www.article-friend.com/'},'fields': 'link'}},
                 media_body=media,
             )
             .execute()
