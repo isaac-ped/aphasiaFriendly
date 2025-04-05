e00b3842c8a59d7343c2a80d2f47e8beccabe823
deb-levy <deborah.levy@princeton.edu>
2025-04-05 10:00:39 -0400

    Update gdocs.py
    tested removing this code
    
    'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
                                            'textStyle': {'italic': True},'fields': 'italic'},
                        'updateParagraphStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
                                                 'paragraphStyle': {'alignment': 'END'},'fields': 'alignment'},
                        'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 21,'endIndex': 35},
                                            'textStyle': {'link': {'url': 'https://www.article-friend.com/'},'fields': 'link'}},

--- readable_af/output/gdocs.py
+++ readable_af/output/gdocs.py
@@ -99,12 +99,6 @@ class GoogleDocGenerator:
                     'createFooter': {'type': 'DEFAULT'},
                     'insertText': {'location': {'segmentId': 'footer','index': 0},
                                    'text': 'Summary created with Article Friend'},
-                    'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
-                                        'textStyle': {'italic': True},'fields': 'italic'},
-                    'updateParagraphStyle': {'range': {'segmentId': 'footer','startIndex': 0,'endIndex': 35},
-                                             'paragraphStyle': {'alignment': 'END'},'fields': 'alignment'},
-                    'updateTextStyle': {'range': {'segmentId': 'footer','startIndex': 21,'endIndex': 35},
-                                        'textStyle': {'link': {'url': 'https://www.article-friend.com/'},'fields': 'link'}},
                 },
                 media_body=media,
             )
